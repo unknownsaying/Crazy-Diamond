bf5d66d868802ee9d264dfdd15d0b8a03faad85c
unknownsaying <15368104568@189.cn>
2026-06-30 21:01:30 +0800

    Update ijkxyzt.vb

--- component/ijkxyzt.vb
+++ component/ijkxyzt.vb
@@ -15,7 +15,7 @@ Function ijkxyz
      ijk() = cos(i) * (x1 - x2) + sin(j) * (y1 + y2) + sin(k) * (z1 + z2) + cos(k) * (z1 - z2)
      ijk() = sin(i) * (x1 + x2) + cos(j) * (y1 - y2) + cos(k) * (z1 - z2) + sin(k) * (z1 + z2)
      ijk() = sin(i) * [(x1 - x2)^2 + (y1 - y2)^2] + cos(j) * [(z1 - z2)^2 + (y1 - y2)^2] +
-                        sin(k) * [(z1 - z2)^2 + (y1 - y2)^2] + cos(k) * [(z1 + z2)^2 + (y1 + y2)^2]
+             sin(k) * [(z1 - z2)^2 + (y1 - y2)^2] + cos(k) * [(z1 + z2)^2 + (y1 + y2)^2]
      ijk() = cos(i) * [(x1 + x2)^2 + (y1 + y2)^2] + sin(j) * [(z1 + z2)^2 + (y1 + y2)^2] +
-                        cos(k) * [(z1 + z2)^2 + (y1 + y2)^2] + sin(k) * [(z1 - z2)^2 + (y1 - y2)^2]
+             cos(k) * [(z1 + z2)^2 + (y1 + y2)^2] + sin(k) * [(z1 - z2)^2 + (y1 - y2)^2]
 End Function
